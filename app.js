25439de4c163183a12b17c84a3d062f620aaff4e
soluzioner <tanimoto@cs.washington.edu>
2014-05-14 16:21:24 +0900

    Basic Express application (supercedes the earlier demo app.js)

--- app.js
+++ app.js
@@ -1,13 +1,59 @@
-/**
- * Created by tanimoto on 5/14/14.
- */
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var http = require('http');
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
-http.createServer( function(req, res) {
-    res.writeHead( 'Content-Type:', 'text/plain');
-    res.end('Hello World! AGAIN and AGAIN!');
-}).listen(3210, '127.0.0.1');
-console.log('Server running at 127.0.0.1:3210');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 
+module.exports = app;
