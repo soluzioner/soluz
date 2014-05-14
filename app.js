c89e6eeb689409522630e05e2ec203ef78ec1614
soluzioner <tanimoto@cs.washington.edu>
2014-05-14 16:01:36 +0900

    Added app.js  as per tutorial by Adron. Trying again.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var http = require('http');
 
 http.createServer( function(req, res) {
     res.writeHead( 'Content-Type:', 'text/plain');
-    res.end('Hello World!');
+    res.end('Hello World! AGAIN');
 }).listen(3210, '127.0.0.1');
 console.log('Server running at 127.0.0.1:3210');
 
